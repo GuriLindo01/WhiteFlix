0f98d826856c288cc8bcf5e7446b942fe00aca96
Gustavo <gustavo.blazius@escola.pr.gov.br>
2025-07-04 10:50:06 -0300

    a

---
+++ main.js
@@ -0,0 +1,7 @@
+function inserirNome(){
+    let nomeUsuario = prompt("Qual é o teu nick?");
+    let elemento = document.querySelector ("#nome-usuario");
+    elemento.textContent = nomeUsuario;
+}
+
+inserirNome();
